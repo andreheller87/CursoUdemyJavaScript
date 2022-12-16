27c658c9b8cb8b60a7bf966c94e3dd74a796dbb8
andreheller87 <andreheller02@gmail.com>
2022-12-16 13:44:34 -0300

    16/12/2022

--- Inicio/Dados1.js
+++ Inicio/Dados1.js
@@ -1,10 +1,11 @@
+var nome= "Caneta";
 var quantidade = 10;
-var preco = 6.5;
+var preco = 6.4;
 var imposto = 1.5;
 var precoFinal = preco + imposto;
 
-console.log("caneta");
-console.log(quantidade);
-console.log(preco);
-console.log(imposto);
-console.log("Total a pagar: " + precoFinal);
+console.log("Nome: " + nome );
+console.log("Qtd: " + quantidade);
+console.log("Preço: " + preco);
+console.log("Inposto: " + imposto);
+console.log("Preço Final: " + precoFinal)
