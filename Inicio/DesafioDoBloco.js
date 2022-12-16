732111fc2b9a08cd11ebf7f4207f14570869b6c6
andreheller87 <andreheller02@gmail.com>
2022-12-16 13:25:53 -0300

    16/12/2022

--- Inicio/DesafioDoBloco.js
+++ Inicio/DesafioDoBloco.js
@@ -1,21 +1,20 @@
 
     //Bloco de codigos 1
 {
-    console.log(1);
-    console.log(3);
-    console.log(5);
-    console.log(7);
-    console.log(9);
+    console.log("Numero pares: ")
+    for (let i = 0; i <= 10; i++) {
+        if(i %2 == 0) {console.log(i);} 
+    }
+    }
 
-}
     //Bloco de codigo 2 
 {
-    console.log(2);
-    console.log(4);
-    console.log(6);
-    console.log(8);
-    console.log(10);
-}
+    console.log("Numero impares: ")
+    for (let i = 0; i <= 10; i++) {
+        if(i %2 != 0) {console.log(i);} 
+    }
+    }
+
 
 
 console.log("Hello World!! ");
