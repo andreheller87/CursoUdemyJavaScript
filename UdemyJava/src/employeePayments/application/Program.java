3c27578b38588d6daf8a82757dba378e96ad556a
andreheller87 <andreheller02@gmail.com>
2023-07-14 15:15:00 -0300

    update

--- UdemyJava/src/employeePayments/application/Program.java
+++ UdemyJava/src/employeePayments/application/Program.java
@@ -1,17 +1,22 @@
 package employeePayments.application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import employeePayments.entities.Department;
+import employeePayments.entities.HourContract;
 import employeePayments.entities.Worker;
 import employeePayments.entities.enums.WorkerLevel;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		System.out.print("Enter department's name: ");
 		String departmentName = sc.nextLine();
@@ -29,6 +34,26 @@ public class Program {
 
 		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), workerBaseSalary,
 				new Department(departmentName));
+		
+		
+		System.out.print("How many contracts to this worker? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i <= n; i++) {
+			
+			System.out.println("Enter contract #"+ i + " data:");
+			
+			System.out.print("Date(DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(sc.next());			
+			System.out.print("Value per hour");
+			double valuePerhour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
+			HourContract contract = new HourContract(contractDate, valuePerhour,hours);
+			worker.addContract(contract);
+
+			
+		}
 
 		
 		sc.close();
