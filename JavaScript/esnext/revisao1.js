b0f3bfccb35894fabe12356cba87f51d9a568df2
andreheller87 <andreheller02@gmail.com>
2023-07-17 13:40:10 -0300

    defaultAndRest

--- JavaScript/esnext/revisao1.js
+++ JavaScript/esnext/revisao1.js
@@ -24,3 +24,35 @@ console.log(x,y)
 const { idade:i, nome} = {nome: 'Ana', idade:5}
 
 console.log(nome, i)
+
+//Arrow Function
+
+const soma =(a,b) => a + b
+
+console.log(soma(2,3))
+
+// Arrow Function (this)
+
+const lexico1 = () => console.log(this === exports)
+
+const lexico2 = lexico1.bind({})
+
+lexico1()
+lexico2()
+
+//parametro default
+function log(texto = 'Node'){
+    console.log(texto)
+}
+
+log()
+log('Sou mais forte');
+
+//operador rest
+
+function total(...numeros){
+    let total =0
+    numeros.forEach(n => total +=n)
+    return total
+}
+console.log(total(2,3,4,5))
