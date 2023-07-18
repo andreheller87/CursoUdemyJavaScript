7bf0ae3653a52b0855dea8158e5fe17db3cd8a25
andreheller87 <andreheller02@gmail.com>
2023-07-18 11:21:35 -0300

    create Program to Account

--- UdemyJava/src/heranca/application/Program.java
+++ UdemyJava/src/heranca/application/Program.java
@@ -1,15 +1,41 @@
 package heranca.application;
 
+import heranca.entites.Account;
 import heranca.entites.BusinessAccount;
+import heranca.entites.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		BusinessAccount businessAccount = new BusinessAccount(001, "Andre Heller",0.0, 1000.0);
-		
-		System.out.println(businessAccount);
+
+		Account acc = new Account(1001, "Alex", 0.0);
+
+		BusinessAccount bacc = new BusinessAccount(1002, "Andre Heller", 0.0, 500.0);
+
+		// UpCasting
+
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 300.0);
+
+		// DownCasting
+
+		BusinessAccount acc4 = (BusinessAccount) acc2;
+		acc4.loan(200.0);
+
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount) acc3;
+			acc5.loan(300.0);
+			System.out.println("Loan!");
+		}
+
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount) acc3;
+			acc5.updateBalance();
+			System.out.println("Update");
+		}
+
+		System.out.println();
 
 	}
 
