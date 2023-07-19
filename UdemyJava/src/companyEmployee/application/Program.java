967b4ac1bc39ca76fdd1d3f1700355d9927fa9df
andreheller87 <andreheller02@gmail.com>
2023-07-19 12:20:49 -0300

    create class Program

--- UdemyJava/src/companyEmployee/application/Program.java
+++ UdemyJava/src/companyEmployee/application/Program.java
@@ -1,10 +1,53 @@
 package companyEmployee.application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import companyEmployee.entities.Employee;
+import companyEmployee.entities.OutsourcedEmployee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<Employee> list = new  ArrayList<>();
+		
+		System.out.println("Enter the number of employees: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Employee #" + i + " data: ");
+			System.out.print("Outsourced (y/n)?");
+			char ch = sc.next().charAt(0);
+			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			
+			System.out.print("Value per hours: ");
+			double valuePerHour = sc.nextDouble();
+			
+			if(ch == 'y') {
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				Employee emp = new OutsourcedEmployee(name,hours, valuePerHour, additionalCharge);
+				list.add(emp);
+			}else {
+				Employee emp = new Employee(name,hours, valuePerHour);
+				list.add(emp);
+			}
+			
+			
+			
+		}
+		
+		sc.close();
 	}
 
 }
