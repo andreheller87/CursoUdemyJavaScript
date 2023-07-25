988010ee2ae506b1c3300e4f377383f22a8849a0
andreheller87 <andreheller02@gmail.com>
2023-07-25 12:52:18 -0300

    update format

--- UdemyJava/src/incomeTax/entities/Individual.java
+++ UdemyJava/src/incomeTax/entities/Individual.java
@@ -24,18 +24,18 @@ public class Individual extends TaxPayer {
 	@Override
 	public double tax() {
 		double basicTax;
-		if(getAnualIncome() < 20000.0) {
+		if (getAnualIncome() < 20000.0) {
 			basicTax = getAnualIncome() * 0.15;
-		}else {
-			basicTax  = getAnualIncome() * 0.25;
+		} else {
+			basicTax = getAnualIncome() * 0.25;
 		}
-		
-		basicTax -= getHealthExpenditures()*0.5;
-		
-		if(basicTax < 0.0) {
-			return  0.0;
-		}else {
-		return basicTax;
+
+		basicTax -= getHealthExpenditures() * 0.5;
+
+		if (basicTax < 0.0) {
+			return 0.0;
+		} else {
+			return basicTax;
 		}
 	}
 
