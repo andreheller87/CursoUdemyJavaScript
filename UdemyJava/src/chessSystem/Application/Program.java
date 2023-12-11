05ce37e72c735ff8d26fe9ef18d282f898b9427f
andreheller87 <andreheller02@gmail.com>
2023-12-11 17:03:43 -0300

    update

--- UdemyJava/src/chessSystem/Application/Program.java
+++ UdemyJava/src/chessSystem/Application/Program.java
@@ -5,7 +5,7 @@ import chessSystem.boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
-		Position position = new Position(1, 5);
+		Position position = new Position(1, 6);
 		
 		System.out.println(position.toString());
 
