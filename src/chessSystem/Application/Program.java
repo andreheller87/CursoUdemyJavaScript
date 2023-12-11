0b8407561061572dd4ac5f19e3a688933a9c9ea7
andreheller87 <andreheller02@gmail.com>
2023-12-11 15:17:17 -0300

    projeto

---
+++ src/chessSystem/Application/Program.java
@@ -0,0 +1,10 @@
+package chessSystem.Application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/orderCycle/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package orderCycle.entities.enums;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
