67a188bcdb2febff78be45a7daff913c255829e2
andreheller87 <andreheller02@gmail.com>
2023-12-11 16:15:08 -0300

    First class Position

--- src/chessSystem/Application/Program.java
+++ src/chessSystem/Application/Program.java
@@ -1,9 +1,13 @@
 package chessSystem.Application;
 
+import chessSystem.boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Position position = new Position(1, 5);
+		
+		System.out.println(position.toString());
 
 	}
 
