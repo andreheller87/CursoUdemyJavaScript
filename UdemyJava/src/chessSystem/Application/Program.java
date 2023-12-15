b8c5b22ff7b08197eb8e28c9294f46a4ad5beecb
andreheller87 <andreheller02@gmail.com>
2023-12-15 15:25:28 -0300

    little improvement in board printing

--- UdemyJava/src/chessSystem/Application/Program.java
+++ UdemyJava/src/chessSystem/Application/Program.java
@@ -4,6 +4,8 @@ import chessSystem.Chess.ChessMatch;
 
 public class Program {
 
+	// git // java chessSystem/Application/Program
+
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch = new ChessMatch();
