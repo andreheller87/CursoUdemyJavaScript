840e41e932cac9e78b955da2de27c652aa32a035
andreheller87 <andreheller02@gmail.com>
2023-12-26 12:08:05 -0300

    Create classe Seller

--- UdemyJava/src/Connector2BancoDeDados/application/ProgramMain.java
+++ UdemyJava/src/Connector2BancoDeDados/application/ProgramMain.java
@@ -1,12 +1,19 @@
 package Connector2BancoDeDados.application;
 
+import java.util.Date;
+
 import Connector2BancoDeDados.model.entities.Department;
+import Connector2BancoDeDados.model.entities.Seller;
 
 public class ProgramMain {
 
 	public static void main(String[] args) {
 		Department d1 = new Department(1,"Books");
+		
+		Seller seller = new Seller(1,"João","joao@gmail.com",new Date(),3000.0,d1);
+		
         System.out.println(d1);
+        System.out.println(seller);
 	}
 
 }
